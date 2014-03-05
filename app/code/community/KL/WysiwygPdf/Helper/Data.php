c0518282ed992d293513a86160b8120bc67f5594
Erik Eng <mail@ptz0n.se>
2014-03-05 11:46:05 +0100

    Handle multiple PDF links in attribute.

--- app/code/community/KL/WysiwygPdf/Helper/Data.php
+++ app/code/community/KL/WysiwygPdf/Helper/Data.php
@@ -13,13 +13,17 @@ class KL_WysiwygPdf_Helper_Data extends Mage_Core_Helper_Abstract
     {
         $dom = new DOMDocument;
         @$dom->loadHTML($data);
+
+        $hrefs = array();
         if (strstr($data, 'href')) {
             foreach ($dom->getElementsByTagName('a') as $tag) {
-                $href = $tag->getAttribute('href');
+                $hrefs[] = $tag->getAttribute('href');
             }
-        } else {
-            $href = $data;
         }
-        return $href;
+        else {
+            $hrefs[] = $data;
+        }
+
+        return $hrefs;
     }
-} 
+}
